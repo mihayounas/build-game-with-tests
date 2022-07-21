4e2ad66c45d39ee13d36656b286b4b61e2b5a323
mihayounas <mihaelayounas55@gmail.com>
2022-07-21 10:18:03 +0000

    Add more functions for the game and test

--- scripts/game.js
+++ scripts/game.js
@@ -22,8 +22,17 @@ function addTurn() {
     game.playerMoves = []
     game.currentGame.push(game.choices[Math.floor(Math.random() * 4)])
 }
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function() {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
 module.exports = {
     game,
     newGame,
-    showScore
+    showScore,
+    addTurn,
+    lightsOn
 }
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,7 +5,9 @@
 const {
     game,
     newGame,
-    showScore
+    showScore,
+    addTurn,
+    lightsOn
 } = require("../game");
 
 
@@ -56,3 +58,26 @@ describe("newGame works correctly", () => {
         expect(document.getElementById("score").innerText).toEqual(0)
     })
 })
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain(game.currentGame[0] + "light");
+    });
+});
